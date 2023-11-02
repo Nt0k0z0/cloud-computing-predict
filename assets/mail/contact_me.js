604e35083f98fcec0b8fc526b8bf0c8162eaf7c0
Nt0k0z0 <133776714+Nt0k0z0@users.noreply.github.com>
2023-11-02 18:48:17 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "{https://eom2mqwyi5.execute-api.eu-west-2.amazonaws.com/ntokozo_dlalisa_function}",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
