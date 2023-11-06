1eb6d80867fc0d541531ba0d0734ce406b765079
Nt0k0z0 <133776714+Nt0k0z0@users.noreply.github.com>
2023-11-06 19:52:46 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  --------
-                url: "https://ql9d983l19.execute-api.eu-west-2.amazonaws.com/default/ntokozo_lambda",
+                url: "https://b77kthagmc.execute-api.eu-west-2.amazonaws.com/default/ntokozo_lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
